7abf170efaa93d12b3ed7b7c883347db6a657dbb
Noakai Aronesty <74675547+naronesty@users.noreply.github.com>
2020-12-11 10:16:44 -0500

    Updated documentation to include summary of class

--- Impts.java
+++ Impts.java
@@ -4,6 +4,20 @@
  *  Execution:    java Impts
  *
  *  A class containing some universally useful functions
+ *  Functions:
+ *  Impts.printArray(array) // Prints an array
+ *  Impts.randInt(int0, int1) // Returns a random integer >= int0 and < int1
+ *  Impts.isNumber(string) // Returns true if a string is a number
+ *  Impts.createIntegerArray(int0 ... int) // Returns an int[] from a list of int arguments
+ *  Impts.reverseArray(array) // Returns an array that is in the opposite order of the argument
+ *  Impts.sumArray(array) // Returns the sum of all elements in an array
+ *  Impts.pyDivide(int0, int1) // Returns the result of python-style division of two ints
+ *  Impts.getCommon (array0, array1) // Returns an array of the common elements in two arrays
+ *  Impts.max(array) // Returns the max value in an array
+ *  Impts.min(array) // Returns the min value in an array
+ *  Impts.divisors(int) // Returns an array of factors of a number
+ *  Impts.append(array, object) // Returns the array argument with the object appended to the end: object can be another array
+ *  Impts.countFreq(object, array) // Returns the frequency of an object in an array: object CANNOT be another array
  ******************************************************************************/
 
 import java.util.*;
